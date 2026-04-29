f4c56b1652840e89341e5816437aa9ce12e5a7d7
Viru0804 <dsaviru6@gmail.com>
2026-04-30 00:31:22 +0530

    Optimal Bubble sort

--- ImportantSortingAlgo/BubbleSort.java
+++ ImportantSortingAlgo/BubbleSort.java
@@ -21,6 +21,7 @@ public class BubbleSort {
         
         for(int i=n-1 ; i>=1; i--)
         {
+            int didswap = 0;
             for(int j=0; j<=i-1; j++)
             {
                 if(arr[j]>arr[j+1])
@@ -28,7 +29,12 @@ public class BubbleSort {
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
+                    didswap = 1;
                 }
+            }
+            if(didswap == 0)
+            {
+                break;
             }
         }
         
