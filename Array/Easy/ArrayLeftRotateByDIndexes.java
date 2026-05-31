faedb3b2ca6caaade9a95ea58c06eaf5d9f7957f
Viru0804 <dsaviru6@gmail.com>
2026-05-31 15:39:41 +0530

    Array Left Rotate By D indexes

---
+++ Array/Easy/ArrayLeftRotateByDIndexes.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class ArrayLeftRotateByDIndexes {   
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n;
+        int d=3;
+        System.out.println("Enter the size of the array ");
+        n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        leftrotatearraybyd(arr,n,d);
+    }
+    public static void leftrotatearraybyd(int[] arr, int n, int d){
+        int a=0;
+        reverse(arr,a,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,a,n-1);
+        for(int i=0; i<n; i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+    
+    public static void reverse(int[] arr, int start, int end){
+        
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
